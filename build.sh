54e96a8a9966710ea93cb2a871a7f26af7c00607
Jiwu Jang <cliid@pm.me>
2024-11-01 17:21:58 -0400

    add print statement in hello.pst

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@ antlr4 -Dlanguage=Cpp -no-listener -visitor -o src/generated Pryst.g4
 rm -rf build
 mkdir build
 cd build
-export CC=/usr/bin/clang-20
-export CXX=/usr/bin/clang++-20
-cmake -DCMAKE_C_COMPILER=/usr/bin/clang-20 \
-      -DCMAKE_CXX_COMPILER=/usr/bin/clang++-20 \
-      -DLLVM_DIR=/usr/lib/llvm-20/cmake ..
+# export CC=/usr/bin/clang-20
+# export CXX=/usr/bin/clang++-20
+cmake ..
 make
